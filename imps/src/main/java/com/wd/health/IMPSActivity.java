31769a6bd8d86adff38f32edf98048c97162378b
chuxunya <3434428185@qq.com>
2019-12-20 16:16:27 +0800

    jim

--- imps/src/main/java/com/wd/health/IMPSActivity.java
+++ imps/src/main/java/com/wd/health/IMPSActivity.java
@@ -1,8 +1,10 @@
-package com.wd.imps;
+package com.wd.health;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.wd.imps.R;
 
 public class IMPSActivity extends AppCompatActivity {
 
